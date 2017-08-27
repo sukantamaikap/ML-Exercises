583d6c71d51964cee2e16b8fdf50f47134cd25ff
sukantamaikap <sukanta.maikap@gmail.com>
2017-08-27 20:21:18 +0530

    working graph code added

--- regression/LinearRegression.py
+++ regression/LinearRegression.py
@@ -1,10 +1,11 @@
 import pandas as pd
 import quandl
-import math, datetime
+import math
+from datetime import timedelta
 import numpy as np
-from sklearn import preprocessing, cross_validation, svm
+from sklearn import preprocessing, cross_validation
 from sklearn.linear_model import LinearRegression
-import matplotlib as plt
+import matplotlib.pyplot as plt
 from matplotlib import style
 style.use('ggplot')
 
@@ -22,29 +23,36 @@ def generateData():
 def predict_and_plot():
     df = pd.read_csv('TSLA.csv')
     print('data read from file : ', df.head(), df.shape)
+    df['Date'] = df['Date'].astype('datetime64[ns]')
+    df.set_index('Date', inplace='True')
     # define the features
     df['HL_PCT'] = (df['Adj. High'] - df['Adj. Close']) / df['Adj. Close'] * 100
     df['PCT_Change'] = ((df['Adj. Close'] - df['Adj. Open']) / df['Adj. Open']) * 100
+
+    #filter out the required columns
     df = df[['Adj. Close', 'HL_PCT', 'PCT_Change', "Adj. Volume"]]
 
     # define the label
     forecast_column = 'Adj. Close'
     df.fillna(-9999, inplace=True)
 
-    # define the forecast lenght
+    # define the forecast length
     forecast_out = int(math.ceil(0.01 * len(df)))
     print("forecast lengthh : " + str(forecast_out))
+
+    # define the label
     df['label'] = df[forecast_column].shift(-forecast_out)
-    # df.dropna(inplace=True)
     print('label chosen ...')
     print('shape of the data after adding label : ', str(df.shape))
     print(df.head())
     print(df.tail())
 
-    #define features
+    #define features i.e remove label from the data frame
     X = np.array(df.drop(['label'], 1))
     X = preprocessing.scale(X)
-    X_lately = X[- forecast_out:]
+    #forecast for last "forecast_out" days only, as they do not have a corresponding predictions
+    X_predict_on = X[- forecast_out:]
+    #remove the "forecast out" count from the end to ensure count parity between feature and label
     X = X[:-forecast_out]
 
     # define label
@@ -58,30 +66,24 @@ def predict_and_plot():
     clf = LinearRegression()
     clf.fit(X_train, y_train)
     accuracy = clf.score(X_test, y_test)
-    forecast_set = clf.predict(X_lately)
+    forecast_set = clf.predict(X_predict_on)
     print(forecast_set, accuracy, forecast_out)
 
+    last_date = df.iloc[-1].name
+    modified_date = last_date + timedelta(days=1)
+    date = pd.date_range(last_date, periods=forecast_out, freq='D')
+    df1 = pd.DataFrame(forecast_set, columns=['Forecast'], index=date)
+    df = df.append(df1)
 
-    # df['Forecast'] = np.nan
-    # last_date = df.iloc[-1].name
-    # last_unix = last_date.timestamp()
-    # one_day = 86400
-    # next_unix = last_unix + one_day
-    #
-    # for i in forecast_set:
-    #     next_date = datetime.fromtimestamp(next_unix)
-    #     next_unix += one_day
-    #     df.loc[next_date] = [np.nan for _ in range(len(df.columns)-1)] + [i]
-    #
-    # print(df.head())
-    # print(df.tail())
-    #
-    # df['Adj. Close'].plot()
-    # df['Forecast'].plot()
-    # plt.legend(loc=1)
-    # plt.xlabel('Date')
-    # plt.ylabel('Price')
-    # plt.show()
+    print(df.head())
+    print(df.tail())
 
-# generateData()
+    df['Adj. Close'].plot()
+    df['Forecast'].plot()
+    plt.legend(loc=1)
+    plt.xlabel('Date')
+    plt.ylabel('Price')
+    plt.show()
+
+generateData()
 predict_and_plot()
